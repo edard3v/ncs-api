7369ec8a251e5ffab27650fbb826258942b34fcc
edard3v <edard3v@gmail.com>
2025-04-11 17:37:05 -0500

    init db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 import { IS_PRODUCTION } from "./src/app/config.ts";
 
 export const DATABASE_URL = !IS_PRODUCTION
-  ? "postgres://postgres:edar@localhost:5432/template"
+  ? "postgres://postgres:edar@localhost:5432/ncs"
   : Deno.env.get("DATABASE_URL")!;
 
 export default defineConfig({
--- src/db/models/accounts.ts
+++ src/db/models/accounts.ts
@@ -1,5 +1,5 @@
 import { sql } from "drizzle-orm";
-import { pgTable, text, integer, uuid, boolean } from "drizzle-orm/pg-core";
+import { pgTable, text, uuid, boolean } from "drizzle-orm/pg-core";
 import { Role } from "../enums/Role.ts";
 
 export const accounts = pgTable("accounts", {
@@ -8,12 +8,9 @@ export const accounts = pgTable("accounts", {
     .default(sql`gen_random_uuid()`),
   role: text({ enum: [Role.admin, Role.client, Role.seller] }).default(Role.client),
   email: text().unique().notNull(),
-  name: text(),
-  tel: integer(),
-  country: text(), // recuerda validar con el enum Country
-  img: text(),
-  password: text().notNull(),
   is_active: boolean().notNull().default(true),
+  img_url: text(),
+  password: text().notNull(),
 
   created_at: text().default(sql`(CURRENT_TIMESTAMP)`),
   updated_at: text().$onUpdate(() => sql`(CURRENT_TIMESTAMP)`),
