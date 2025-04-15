72904a01b66e36d9b066988b517745ba21dfccdf
edard3v <edard3v@gmail.com>
2025-04-15 01:40:16 -0500

    cambiando neon de npm a neon jsr

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,14 +1,13 @@
 import pg from "pg";
 import { drizzle as drizzle_remote } from "drizzle-orm/neon-http";
 import { drizzle as drizzle_local } from "drizzle-orm/node-postgres";
+import { neon } from "@neon/serverless";
 import { DATABASE_URL } from "../../drizzle.config.ts";
+import { IS_PRODUCTION } from "../app/config.ts";
 import * as schema from "./schema.ts";
-import { IS_PRODUCTION } from "@app/config.ts";
 // import * as relations from "./relations.ts";
 
+const sql = !IS_PRODUCTION ? new pg.Pool({ connectionString: DATABASE_URL }) : neon(DATABASE_URL);
 const config = { schema: { ...schema } };
-const pg_pool = new pg.Pool({ connectionString: DATABASE_URL });
 
-export const db = !IS_PRODUCTION
-  ? drizzle_local(pg_pool, config)
-  : drizzle_remote(DATABASE_URL, config);
+export const db = !IS_PRODUCTION ? drizzle_local(sql, config) : drizzle_remote(sql, config);
