7afd638d666c02d17c3ba9e3d6d70d8aac813f46
edard3v <edard3v@gmail.com>
2026-06-01 16:03:58 -0500

    centralizando variables de entorno en config

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,13 +1,13 @@
 import { Config, createClient } from "@libsql/client";
-import { ENV } from "@/app/config.ts";
+import { DB_TOKEN, DB_URL, ENV } from "@/app/config.ts";
 
 const local_config = {
   url: "file:/home/edar/databases/ncs/ncs.db",
 };
 
 const production_config = {
-  url: Deno.env.get("DB_URL") || "",
-  authToken: Deno.env.get("DB_TOKEN"),
+  url: DB_URL || "",
+  authToken: DB_TOKEN,
 };
 
 const db_config: Config = ENV == "local" ? local_config : production_config;
