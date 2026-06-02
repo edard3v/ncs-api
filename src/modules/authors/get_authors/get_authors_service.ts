a7ecec3673753f363f892b652e00df87b5c95e74
edard3v <edard3v@gmail.com>
2026-06-02 09:07:46 -0500

    obteniendo autores con sus canciones

--- src/modules/authors/get_authors/get_authors_service.ts
+++ src/modules/authors/get_authors/get_authors_service.ts
@@ -1,41 +1,77 @@
 import { GetAuthorsDto } from "@/modules/authors/get_authors/get_authors_dto.ts";
 import { db } from "@/db/db.ts";
+import { PageErr } from "@/errors/PageErr.ts";
+import { Record404 } from "@/errors/Record404.ts";
 
 export const get_authors_service = async (params: GetAuthorsDto) => {
   const { name, limit = 5, page = 1 } = params;
+
+  if (!page) throw new PageErr();
 
   const conditions: string[] = [];
   const args: string[] = [];
   const offset = (page - 1) * limit;
 
   if (name) {
-    conditions.push("name like ?");
+    conditions.push("authors.name like ?");
     args.push(`%${name}%`);
   }
 
   const where_clause = conditions.length > 0 ? " where " + conditions.join(" and ") : "";
 
-  const count_sql = `select count(*) as total from authors${where_clause}`;
+  const sql_count = `
+  select
+  count(distinct authors.id) as total
+  from authors inner join songs on authors.id = songs.author_id
+  ${where_clause}
+  `;
+
   const count_result = await db.execute({
-    sql: count_sql,
+    sql: sql_count,
     args,
   });
 
   const total_records = Number(count_result.rows[0].total);
-  const total_pages = Math.ceil(total_records / limit);
 
-  const sql = `select id, name, img_url from authors${where_clause} limit ? offset ?`;
+  if (!total_records) throw new Record404();
+
+  const total_pages = Math.ceil(total_records / limit) || 1;
+
+  const sql = `
+  select
+  authors.id,
+  authors.name,
+  authors.img_url,
+  json_group_array(
+    json_object(
+      'id', songs.id,
+      'name', songs.name,
+      'song_url', songs.song_url
+    )
+  ) as songs
+  from authors inner join songs on authors.id = songs.author_id
+  ${where_clause}
+  group by authors.id
+  order by authors.name asc
+  limit ?
+  offset ?
+  `;
 
   const result = await db.execute({
     sql,
     args: [...args, limit, offset],
   });
 
+  const formatted_records = result.rows.map((row) => ({
+    ...row,
+    songs: JSON.parse(row.songs as string),
+  }));
+
   return {
     total_records,
-    total_pages,
     limit,
+    total_pages,
     page,
-    records: result.rows,
+    records: formatted_records,
   };
 };
