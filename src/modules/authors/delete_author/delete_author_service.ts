73c0fffbf0c35c764e37334f6bcef49bd8d064ca
edard3v <edard3v@gmail.com>
2026-06-09 07:02:30 -0500

    eliminar autor "funcional" falta testear

--- src/modules/authors/delete_author/delete_author_service.ts
+++ src/modules/authors/delete_author/delete_author_service.ts
@@ -1,14 +1,23 @@
 import { UuidZod } from "@/services/zod/uuid_zod.ts";
 import { db } from "@/db/db.ts";
 import { Record404 } from "@/errors/Record404.ts";
+import { Cloudinary } from "@/services/cloudinary/cloudinary.ts";
 
 export const delete_author_service = async (id: UuidZod) => {
   const result = await db.execute({
-    sql: "delete from authors where id = ?",
+    sql: "delete from authors where id = ? returning img_url",
     args: [id],
   });
 
   if (!result.rowsAffected) throw new Record404();
+
+  const deleted_author = result.rows[0];
+
+  if (result.rowsAffected) {
+    Cloudinary.destroy(deleted_author.img_url as string, "image").catch((err) => {
+      console.error("Fallo al borrar imagen vieja en Cloudinary):", deleted_author.img_url, err);
+    });
+  }
 
   return result.rowsAffected;
 };
